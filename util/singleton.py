5074569a534143dd1199e839e892f9270fbe4da6
Elad Noor <elad.noor@gmail.com>
2014-04-19 22:53:00 +0300

    spring cleaning - some old APIs which are not used anymore were removed

--- util/singleton.py
+++
@@ -1,8 +0,0 @@
-def Singleton(cls):
-    """Decorator that makes a class a singleton."""
-    instance_container = []
-    def getinstance():
-        if not len(instance_container):
-            instance_container.append(cls())
-        return instance_container[0]
-    return getinstance
