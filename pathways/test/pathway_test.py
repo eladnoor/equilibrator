b131b47a917a849f82c4205137ae639263367d36
Avi Flamholz <flamholz@gmail.com>
2016-08-30 13:48:00 -0400

    Updated test data in new formats.

--- pathways/test/pathway_test.py
+++ pathways/test/pathway_test.py
@@ -1,9 +1,21 @@
-from django.test import TestCase
-
 import pathways
+
+from django.test import TestCase
+from util.SBtab import SBtabTools
+
 
 class PathwayTest(TestCase):
 
-    def test_from_file(self):
-        path = pathways.Pathway.from_filename('pathways/test/simple_pathway.csv')
+    def test_from_csv_file(self):
+    	with open('pathways/test/EMP_glycolysis_to_EtOH_simple.tsv') as f:
+    		path = pathways.ParsedPathway.from_csv_file(f)
+        	path.print_reactions()
+
+    def _notest_from_simple_sbtab(self):
+    	ds = SBtabTools.openMultipleSBtab('pathways/test/EMP_glycolysis_to_EtOH_simple.tsv')
+    	path = pathways.ParsedPathway.from_simple_sbtab(ds[0])
         path.print_reactions()
+
+
+if __name__ == '__main__':
+    unittest.main()
