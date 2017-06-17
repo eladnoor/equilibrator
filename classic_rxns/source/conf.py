e22930256375535f325e14f71cea6bbd444e3976
Avi Flamholz <flamholz@gmail.com>
2017-06-17 15:14:41 -0700

    Add logo.

--- classic_rxns/source/conf.py
+++ classic_rxns/source/conf.py
@@ -105,7 +105,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_logo = '_static/_images/equilibrator_logo_80px.png'
 
 # -- Options for HTMLHelp output ------------------------------------------
 
